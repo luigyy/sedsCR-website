eb312709194514f4a7c2f25eb6e01c252fb43920
luigyy <luigy.valverde@ucr.ac.cr>
2023-01-11 20:22:19 -0600

    landing page mockup

--- src/components/NavbarForHero.tsx
+++ src/components/NavbarForHero.tsx
@@ -5,9 +5,16 @@ interface NavbarForHeroProps {}
 const NavbarForHero: React.FC<NavbarForHeroProps> = ({}) => {
   return (
     <div className=" flex justify-between items-center">
-      <div className="font-semibold text-2xl">
+      <div className="font-semibold text-2xl flex items-center">
         <span className="font-semibold">SEDS</span>
         <span className="text-primary ">CR</span>
+        <span>
+          <img
+            className="px-1 "
+            src="https://flagcdn.com/20x15/cr.png"
+            alt=""
+          />
+        </span>
       </div>
       <div className="">
         <span className="mx-2 bg-primary text-primary-content font-semibold px-4 py-3 rounded-3xl">
